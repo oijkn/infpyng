cba7c9ce1bd23ccf55f116dab1c68c24fa18324c
root <root@PC-213.home>
2020-06-01 16:53:09 +0200

    trying docker-compose stack

--- infpyng-stack/clear_all.sh
+++ infpyng-stack/clear_all.sh
@@ -1,4 +1,6 @@
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker rmi -f $(docker images -q)
+docker-compose --project-name infpyng -f docker-compose.yml down
+docker rmi $(docker images | grep grafana | awk '{print $3}')
+docker rmi $(docker images | grep chronograf | awk '{print $3}')
+docker rmi $(docker images| grep influxdb | awk '{print $3}')
+docker rmi $(docker images | grep infpyng | awk '{print $3}')
 rm -rf data/
--- infpyng-stack/run.sh
+++ infpyng-stack/run.sh
@@ -1,5 +1,7 @@
-mkdir data
-mkdir data/grafana
-chmod a+w data/grafana
+mkdir -p data
+mkdir -p data/grafana
+mkdir -p data/grafana/plugins
+chmod -R a+w data/grafana
 touch data/infpyng.log
+tar -zxf grafana-provisioning/dashboards/plugins/multibar-graph-panel_0.2.5.tar.gz -C data/grafana/plugins/
 docker-compose --project-name infpyng -f docker-compose.yml up -d
